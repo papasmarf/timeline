6320e44c3a861351ade2ca520be56b6527ab3c56
haan <founders@smarf.kr>
2016-05-23 17:43:47 +0900

    마이그레이션 파일과 모델 파일 작성하고 댓글 테이블 만들기

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-	validates	:content, :presence => { :message => "Please write something..." }
+	has_many		:comments
+	validates		:content, :presence => { :message => "Please write something..." }
 end
